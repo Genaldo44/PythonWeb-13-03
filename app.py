beaa9d36eb572185dbd8f5bad9d927fbb8c92058
Danilo de Souza Miguel <danilo.miguel@cromai.com>
2024-03-27 14:02:20 +0000

    Criar a estrutura para trabalhar com a base de dados

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
 
 app  = Flask("Olá")
 
+DATABASE = "banco.bd"
+SECRET_KEY = "1234"
 
-@app.route("/teste")
+app.configure.from_object(__name__)
+
+@app.route("/")
+
+
+
+def conectar():
+    return sqlite3.connect(DATABASE)
+
+def before_request():
+    g.bd =  conectar() 
+
+def teardown_request(f):
+    g.bd.close()
 
 def ola():
     return render_template("hello.html")
