7f860705eb9b1ba7f578591e27e0ba151265ffbf
Danilo de Souza Miguel <danilo.miguel@cromai.com>
2024-03-28 13:27:49 +0000

    Aula 28-03 até o intervalo

--- app.py
+++ app.py
@@ -12,18 +12,26 @@ SECRET_KEY = "1234"
 def conectar():
     return sqlite3.connect(DATABASE)
 
+@app.before_request
 def before_request():
     g.bd =  conectar() 
 
-def teardown_request():
+@app.teardown_request
+def teardown_request(f):
     g.bd.close()
 
 @app.route("/")
-def ola():
-    nomeUsuario = "Danilo"
-    listaUsuario = ["Lorena", "Daniel", "Louise", "Demethrius"]
-    posts = [{"titulo": "Meu titulo", "texto": "Meu texto", "data_criacao":"27/03/2024"},
-             {"titulo": "Meu titulo2", "texto": "Meu texto2", "data_criacao":"26/03/2024"}]
-    return render_template("hello.html", post=posts)
+def exibir_posts():
+    sql = "SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC"
+    resultado = g.bd.execute(sql)
+    post = []
+
+    for titulo, texto, data_criacao in resultado.fetchall():
+        post.append({
+            "titulo":titulo, 
+            "texto":texto, 
+            "data_criacao":data_criacao
+           })
+    return render_template("exibir_posts.html", post=post)
 
 
